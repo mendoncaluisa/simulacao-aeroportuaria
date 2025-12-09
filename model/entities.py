bece4d77e1748713a1f82fefe8ebb6de49a8afd3
Maria Luísa Mendonça Oliveira <0049545@academico.ifmg.edu.br>
2025-12-08 22:06:01 -0300

    entidade passageiro

--- model/entities.py
+++ model/entities.py
@@ -1,6 +1,5 @@
 # Eventos associados ao passageiro:
 #   # chegada do pedido de reserva
-#   # cancelamento
 #   # no-show
 #   # check-in (presença confirmada)
 #   # tentativa de embarque
@@ -8,14 +7,35 @@
 #
 # Estado do passageiro:
 #   # reservado
-#   # cancelado
 #   # presente
 #   # no-show
 #   # embarcado
 #   # preterido (embarque negado)
 
+import simpy
+import random
 
-class Passageiro:
-    def __init__(self, id):
+class Passageiro(object):
+    def __init__(self, env, id, p_noshow_pre_checkin, p_noshow_pos_checkin):
+        self.env = env
         self.id = id
-        self.status = ""
+        self.p_noshow_pre_checkin = p_noshow_pre_checkin
+        self.p_noshow_pos_checkin = p_noshow_pos_checkin
+        self.status = "reservado"
+
+        # Processo principal
+        self.action = env.process(self.run())
+
+    def processar(self):
+        # Possibilidade de noshow pre checkin
+        if random.random() < self.p_noshow_pre_checkin:
+            yield self.env.timeout(random.randint(1, 50))
+            self.status = "no-show"
+            return
+
+        # Possibilidade de noshow pre checkin
+        if random.random() < self.p_noshow_pos_checkin:
+            self.status = "no-show"
+            return
+
+
